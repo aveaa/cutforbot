dde5dd8ca837e77ecfec10e9d211255e3579216a
Revolier <46267789+Revolier@users.noreply.github.com>
2019-01-26 14:58:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -56,40 +56,6 @@ if(command === 'help') {
     }
   }
 });
-
-	if(command === 'osu') {
-    message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
-
-	
-	
 }
     if(command === 'embed') {
       if (!message.author.id === '514403823184248833' || !message.author.id === '361951318929309707') return message.channel.send('nuu')
