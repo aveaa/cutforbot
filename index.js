0b91c038c7cc671dfc1afb36f8ede4d8b912a0e3
Revolier <46267789+Revolier@users.noreply.github.com>
2019-01-26 15:29:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -33,26 +33,17 @@ if(command === 'help') {
       name: client.user.username,
       icon_url: client.user.avatarURL
     },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
+    title: "Команды Yuki",
+    url: "https://osu.ppy.sh/",
+    description: "Дополнительная информация: [{
+        name: "Fun:",
+        value: "tickle cuddle pat hug poke kiss"
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "© Example"
+      text: "Yuki Server"
     }
   }
 });
